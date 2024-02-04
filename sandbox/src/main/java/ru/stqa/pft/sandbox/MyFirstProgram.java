2408c9f2600e2807616a17b9bbe7be229de32f99
KyK1oVod <dulos92@gmail.com>
2024-02-05 00:03:35 +0300

    Проект "отвязан" от репозитория Git

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,9 +1,8 @@
 package ru.stqa.pft.sandbox;
 
-public class MyFirstProgram
-{
+public class MyFirstProgram {
 
-   public static void main(String[] args) {
-       System.out.println("HelloWorld");
-   }
+  public static void main(String[] args) {
+    System.out.println("HelloWorld");
+  }
 }
